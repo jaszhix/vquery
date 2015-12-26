b1756cb507b6d7b98242e0a58821c8a5b033c4c4
Jason Hicks <jaszhix@gmail.com>
2015-12-26 01:33:36 -0600

    Update nodes

--- src/index.js
+++ src/index.js
@@ -1,142 +1,153 @@
 export default function (selector) {
-  var methods = {
-    nodes: document.querySelectorAll(selector),
-    node: document.querySelector(selector),
-    ready: function(func) {
+  return {
+    nodes: () => {
+      if (typeof selector === 'string') {
+        return document.querySelectorAll(selector);
+      } else {
+        return selector;
+      }
+    },
+    node: () => {
+      if (typeof selector === 'string') {
+        return document.querySelectorAll(selector);
+      } else {
+        return selector;
+      }
+    },
+    ready: function (func) {
       document.addEventListener('DOMContentLoaded', func);
     },
-    load: function(func) {
+    load: function (func) {
       window.attachEvent( 'onload', func );
     },
-    hide: function() {
-      for (var i = this.nodes.length - 1; i >= 0; i--) {
-        this.nodes[i].style.display = 'none';
+    hide: function () {
+      for (var i = this.nodes().length - 1; i >= 0; i--) {
+        this.nodes()[i].style.display = 'none';
       }
       return this;
     },
-    show: function() {
-      for (var i = this.nodes.length - 1; i >= 0; i--) {
-        this.nodes[i].style.display = 'block';
+    show: function () {
+      for (var i = this.nodes().length - 1; i >= 0; i--) {
+        this.nodes()[i].style.display = 'block';
       }
       return this;
     },
-    remove: function() {
-      for (var i = this.nodes.length - 1; i >= 0; i--) {
-        this.nodes[i].parentNode.removeChild(this.nodes[i]);
+    remove: function () {
+      for (var i = this.nodes().length - 1; i >= 0; i--) {
+        this.nodes()[i].parentNode.removeChild(this.nodes()[i]);
       }
       return this;
     },
-    empty: function() {
-      for (var i = this.nodes.length - 1; i >= 0; i--) {
-        this.nodes[i].innerHTML = '';  
+    empty: function () {
+      for (var i = this.nodes().length - 1; i >= 0; i--) {
+        this.nodes()[i].innerHTML = '';  
       }
       return this; 
     },
-    /*clone: function() {
-      for (var i = this.nodes.length - 1; i >= 0; i--) {
-        this.nodes[i].cloneNode(true);   
+    /*clone: function () {
+      for (var i = this.nodes().length - 1; i >= 0; i--) {
+        this.nodes()[i].cloneNode(true);   
       }
       return this;
     },*/
-    wrap: function (){
-      for (var i = this.nodes.length - 1; i >= 0; i--) {
-        while (this.nodes[i].firstChild) {
-          this.nodes[i].removeChild(this.nodes[i].firstChild);
+    wrap: function () {
+      for (var i = this.nodes().length - 1; i >= 0; i--) {
+        while (this.nodes()[i].firstChild) {
+          this.nodes()[i].removeChild(this.nodes()[i].firstChild);
         }
       }
       return this;
     },
-    parent: function() {
-      this.node.parentNode;
+    parent: function () {
+      this.node().parentNode;
       return this;
     },
-    isEmpty: function() {
-      return !this.node.hasChildNodes(); 
+    isEmpty: function () {
+      return !this.node().hasChildNodes(); 
     },
-    next: function() {
-      this.node.nextElementSibling;
+    next: function () {
+      this.node().nextElementSibling;
       return this;
     },
-    addClass: function(_class) {
-      for (var i = this.nodes.length - 1; i >= 0; i--) {
-        this.nodes[i].classList.add(_class);
+    addClass: function (_class) {
+      for (var i = this.nodes().length - 1; i >= 0; i--) {
+        this.nodes()[i].classList.add(_class);
       }
       return this;
     },
-    removeClass: function(_class) {
-      for (var i = this.nodes.length - 1; i >= 0; i--) {
-        this.nodes[i].classList.remove(_class);
+    removeClass: function (_class) {
+      for (var i = this.nodes().length - 1; i >= 0; i--) {
+        this.nodes()[i].classList.remove(_class);
       }
       return this;
     },
-    toggleClass: function(_class) {
-      for (var i = this.nodes.length - 1; i >= 0; i--) {
-        this.nodes[i].classList.toggle(_class);
+    toggleClass: function (_class) {
+      for (var i = this.nodes().length - 1; i >= 0; i--) {
+        this.nodes()[i].classList.toggle(_class);
       }
       return this;
     },
-    hasClass: function(_class) {
-      var bool = this.node.classList.contains(_class);
+    hasClass: function (_class) {
+      var bool = this.node().classList.contains(_class);
       return bool;
     },
-    attr: function(key, value) {
-      for (var i = this.nodes.length - 1; i >= 0; i--) {
-        this.nodes[i].setAttribute(key, value);
+    attr: function (key, value) {
+      for (var i = this.nodes().length - 1; i >= 0; i--) {
+        this.nodes()[i].setAttribute(key, value);
       }
       return this;
     },
-    css: function(props) {
-      for (var i = this.nodes.length - 1; i >= 0; i--) {
+    css: function (props) {
+      for (var i = this.nodes().length - 1; i >= 0; i--) {
         for (var y in props) {
-          this.nodes[i].style[y] = props[y];
+          this.nodes()[i].style[y] = props[y];
         }
       }
       return this;
     },
-    click: function(func) {
-      for (var i = this.nodes.length - 1; i >= 0; i--) {
-        this.nodes[i].addEventListener('click', func);
+    click: function (func) {
+      for (var i = this.nodes().length - 1; i >= 0; i--) {
+        this.nodes()[i].addEventListener('click', func);
       }
       return this;
     },
-    html: function(contents) {
+    html: function (contents) {
       var output = [];
-      for (var i = this.nodes.length - 1; i >= 0; i--) {
+      for (var i = this.nodes().length - 1; i >= 0; i--) {
         if (!contents) {
-          output.push(this.nodes[i].innerHTML);
+          output.push(this.nodes()[i].innerHTML);
         } else {
-          this.nodes[i].innerHTML = contents;
+          this.nodes()[i].innerHTML = contents;
         }
       }
       return output;
     },
-    text: function(contents) {
+    text: function (contents) {
       var output = [];
-      for (var i = this.nodes.length - 1; i >= 0; i--) {
+      for (var i = this.nodes().length - 1; i >= 0; i--) {
         if (!contents) {
-          output.push(this.nodes[i].textContent);
+          output.push(this.nodes()[i].textContent);
         } else {
-          this.nodes[i].textContent = contents;
+          this.nodes()[i].textContent = contents;
         }
       }
       return output;
     },
-    prepend: function(selectedElement) {
-      for (var i = this.nodes.length - 1; i >= 0; i--) {
-        this.nodes[i].insertBefore(selectedElement, this.nodes[i].firstChild);
+    prepend: function (selectedElement) {
+      for (var i = this.nodes().length - 1; i >= 0; i--) {
+        this.nodes()[i].insertBefore(selectedElement, this.nodes()[i].firstChild);
       }
       return this;
     },
-    append: function(element) {
-      for (var i = this.nodes.length - 1; i >= 0; i--) {
-        this.nodes[i].appendChild(element);
+    append: function (element) {
+      for (var i = this.nodes().length - 1; i >= 0; i--) {
+        this.nodes()[i].appendChild(element);
       }
       return this;
     },
-    contains: function(text) {
-      var bool = this.node.textContent.indexOf(text) > -1;
+    contains: function (text) {
+      var bool = this.node().textContent.indexOf(text) > -1;
       return bool;
     }
   };
-  return methods;
 }
